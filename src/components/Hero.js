f71b2f46554d0e675f941b5666f86921c462c77a
Kushagr Narayan <nkushagr@gmail.com>
2023-04-23 20:27:41 +0530

    navbar_2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,17 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      <div>
-        This is regular
-      </div>
-      <br/>
-      <div className='font-Blinker'>
-    This is Blinker
-      </div>
-      <div className='font-Blinker font-bold text-6xl'>
-    This is Blinker
-      </div>
+    <div className='bg-hero-bgs h-screen w-full bg-no-repeat bg-cover bg-center '>
     </div>
   )
 }
