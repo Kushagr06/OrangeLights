8dbe6964f9d283bac60230aafd4a896b056e7bff
Kushagr Narayan <nkushagr@gmail.com>
2023-04-25 02:12:02 +0530

    about_finished

--- src/components/HeroCar.js
+++ src/components/HeroCar.js
@@ -1,32 +1,38 @@
 import React from 'react'
+import office from '../assets/office_pics/office.jpg'
+import wedding from '../assets/office_pics/weddinglights.jpg'
+import street from '../assets/office_pics/streetlight.jpg'
+import rest from '../assets/office_pics/restaurant_lights.png'
+import land from '../assets/office_pics/land.png'
+import out from '../assets/office_pics/outside.png'
 // https://unsplash.it/200/200
 
 function HeroCar() {
   return (
 <div>
 <section className="text-gray-600 body-font mb-2">
-  <div className="container px-5 py-24 mx-auto flex flex-wrap">
+  <div className="container px-5 py-12 mx-auto flex flex-wrap">
     <div className="flex flex-wrap md:-m-2 -m-1">
       <div className="flex flex-wrap w-1/2">
         <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src={office} />
         </div>
         <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301" />
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src={wedding} />
         </div>
         <div className="md:p-2 p-1 w-full">
-          <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360" />
+          <img alt="gallery" className="w-full h-full object-cover object-center block" src={street}/>
         </div>
       </div>
       <div className="flex flex-wrap w-1/2">
         <div className="md:p-2 p-1 w-full">
-          <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361" />
+          <img alt="gallery" className="w-full h-full object-cover object-center block" src={rest} />
         </div>
         <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302" />
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src={land} />
         </div>
         <div className="md:p-2 p-1 w-1/2">
-          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303" />
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src={out} />
         </div>
       </div>
     </div>
