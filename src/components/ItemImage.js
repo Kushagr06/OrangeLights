ba7b060cf1b08ead6a5d8b8897c82fd30089e55b
Kushagr Narayan <nkushagr@gmail.com>
2023-04-26 12:45:21 +0530

    items

--- src/components/ItemImage.js
+++ src/components/ItemImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ItemImage() {
   return (
     <div>
-      
+          <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400" />
     </div>
   )
 }
