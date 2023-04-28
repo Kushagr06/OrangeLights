7556c5c82a9c9d43fd786faf8ffc546edbe025fb
Kushagr Narayan <nkushagr@gmail.com>
2023-04-29 02:37:06 +0530

    routing

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+
+import AboutUs from "./components/AboutUs";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Item from "./components/Item";
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+
+
+
+
+function Home() {
+  return (
+    <div className="scroll-smooth">    
+      <Navbar />
+      <div id="Hero">
+      <Hero />
+      </div>
+      <div id="About">
+      <AboutUs />
+      </div>
+      <div id="Products">
+      <Products /> 
+      </div>
+      <div id="Find">   
+      <Footer />
+      </div>  
+      {/* <Item /> */}
+    </div>
+  );
+}
+
+export default Home;
