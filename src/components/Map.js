3a5f02e914d22d7e37e16a951acf531dd19769b6
Kushagr Narayan <nkushagr@gmail.com>
2023-05-08 00:47:00 +0530

    map

---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const location = {
+  address: 'V. Light Industries',
+  lat: 19.196301,
+  lng: 72.954491,
+} 
+
+function Map() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Map
